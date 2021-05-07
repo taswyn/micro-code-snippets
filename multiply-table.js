b1629d3b673ef6f801796e6ab292335ab1ea1678
Kaitlyn Goodall <kgoodall@montana.edu>
2021-05-07 15:07:16 -0600

    remove console.loggggging

--- multiply-table.js
+++ multiply-table.js
@@ -1,6 +1,5 @@
 ((multiplyBy = 1) => {
     const tableCells = document.querySelectorAll('.content tbody td');
-    console.log(tableCells);
     for (tableCell of tableCells) {
         tableCell.innerText = tableCell.innerText.match(/[0-9]/g) ? parseInt(parseInt(tableCell.textContent.replace(/[^0-9\.]/g, "")) * multiplyBy) : "";
     }
